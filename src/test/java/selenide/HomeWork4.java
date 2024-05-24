946d77a8bd2b5958f8fb32cc83f96f5c700b95a0
Eryilyvatar <hess04@tut.by>
2024-05-24 21:01:37 +0300

    HomeWork 3

---
+++ src/test/java/selenide/HomeWork4.java
@@ -0,0 +1,20 @@
+package selenide;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.conditions.Text;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class HomeWork4 {
+    @Test
+    void requiredPageHasLoaded () {
+        Configuration.holdBrowserOpen = true;
+        open("https://github.com/");
+        $(byText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+        $(byText("The AI-powered")).shouldHave(text("developer platform."));
+    }
+}
