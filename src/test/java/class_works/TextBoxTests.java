fa5f83b794ff1cee1c2b4232d4fd6fa3549578ca
Eryilyvatar <hess04@tut.by>
2024-06-13 23:13:08 +0300

    new TextBoxTests

--- src/test/java/class_works/TextBoxTests.java
+++ src/test/java/class_works/TextBoxTests.java
@@ -1,4 +1,41 @@
 package class_works;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
 public class TextBoxTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "2560x1440";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fiilFormTest() {
+
+        String userName = "Teslyuk Vadim";
+        String userEmail = "hess04@tut.by";
+        String currentAddress = "Some address 1";
+        String permanentAddress = "Other address 2";
+
+        open("/text-box");
+        $(".text-center").shouldHave(text("Text Box"));
+        $("#userName").setValue(userName);
+        $("#userEmail").setValue(userEmail);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
+        $("#submit").click();
+
+        actions().moveToElement($("#output").shouldBe(visible));
+        $("#output #name").shouldHave(text(userName));
+        $("#output #email").shouldHave(text(userEmail));
+        $("#output #currentAddress").shouldHave(text(currentAddress));
+        $("#output #permanentAddress").shouldHave(text(permanentAddress));
+    }
 }
