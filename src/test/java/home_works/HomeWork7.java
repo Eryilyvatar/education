6027f899ee34064297a0e29760c4cd3c961055d3
VadimTeslyuk <eryilyvatar@gmail.com>
2024-09-24 22:06:54 +0300

    13 work with files

--- src/test/java/home_works/HomeWork7.java
+++ src/test/java/home_works/HomeWork7.java
@@ -1,31 +1,13 @@
 package home_works;
 
 import org.junit.jupiter.api.Test;
-import java.io.File;
+
 
 public class HomeWork7 extends TestBase {
 
 
     @Test
     void formVerification() {
-
-        String firstName = "Miroslav";
-        String lastName = "Hess";
-        String userEmail = "hess04@tut.by";
-        String userGender = "Male";
-        String userNumber = "2965842365";
-        String monthBirthUser = "October";
-        String yearBirthUser = "1989";
-        String dayBirthUser = "10";
-        String userSubjects = "Math";
-        String hobbie1 = "Sports";
-        String hobbie2 = "Reading";
-        String hobbie3 = "Music";
-        String pictureUrl = "my_nero_picture.jpg";
-        String userAddress = "8150 Padberg Mountain";
-        String userState = "Uttar Pradesh";
-        String userCity = "Agra";
-
 
         registrationPage.openPage()
                 .setFirstName(firstName)
--- src/test/java/home_works/TestBase.java
+++ src/test/java/home_works/TestBase.java
@@ -6,6 +6,22 @@ import pages.RegistrationPage;
 
 public class TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
+    String firstName = "Miroslav";
+    String lastName = "Hess";
+    String userEmail = "hess04@tut.by";
+    String userGender = "Male";
+    String userNumber = "2965842365";
+    String monthBirthUser = "October";
+    String yearBirthUser = "1989";
+    String dayBirthUser = "10";
+    String userSubjects = "Math";
+    String hobbie1 = "Sports";
+    String hobbie2 = "Reading";
+    String hobbie3 = "Music";
+    String pictureUrl = "my_nero_picture.jpg";
+    String userAddress = "8150 Padberg Mountain";
+    String userState = "Uttar Pradesh";
+    String userCity = "Agra";
 
     @BeforeAll
     static void beforeAll() {
